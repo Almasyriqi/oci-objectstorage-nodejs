0528fc3b059b2cd10bac8b325d58219e2c6c4e5b
Almasyriqi <ikramalmasyriqi@gmail.com>
2021-12-04 14:29:12 +0700

    coba lagi

--- func.js
+++ func.js
@@ -2,7 +2,6 @@ const fdk=require('@fnproject/fdk');
 const objectStore = require( './saveObject.js' );
 
 fdk.handle(function(input){
-  objectStore.runShippingExtractionJob("NewFile.json", { content: "My Name is M. Syifaul Ikrom Almasyriqi", moreContent: "From Class TI-3G", value: 28 })
   let name = 'ObjectStorage';
   if (input.name) {
     name = input.name;
--- saveObject.js
+++ saveObject.js
@@ -49,4 +49,4 @@ module.exports = {
 }
 
 // test call
-runShippingExtractionJob("BrandNewFile.json", { content: "My Name is M. Syifaul Ikrom Almasyriqi", moreContent: "From Class TI-3G", value: 20 })
+runShippingExtractionJob("NewFile.json", { content: "My Name is M. Syifaul Ikrom Almasyriqi", moreContent: "From Class TI-3G", value: 21 })
